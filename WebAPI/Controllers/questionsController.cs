82ec421ecbea22f87c91519343fade92aa249d49
Shubham Saxena <shubham0987@gmail.com>
2015-07-21 15:30:20 +0530

    Initial Commit

---
+++ WebAPI/Controllers/questionsController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class questionsController : ApiController
+    {
+        private WebApiDBEntities db = new WebApiDBEntities();
+
+        // GET: api/questions
+        public IQueryable<question> Getquestions()
+        {
+            return db.questions;
+        }
+
+        // GET: api/questions/5
+        [ResponseType(typeof(question))]
+        public IHttpActionResult Getquestion(int id)
+        {
+            question question = db.questions.Find(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(question);
+        }
+
+        // PUT: api/questions/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putquestion(int id, question question)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != question.quesID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(question).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!questionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/questions
+        [ResponseType(typeof(question))]
+        public IHttpActionResult Postquestion(question question)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var id = 0;
+            try
+            {
+                id = db.questions.Max(p => p.quesID) + 1;
+            }
+            catch(Exception exp)
+            {
+                id = 1;
+            }
+            
+            question.postedOn = DateTime.Now;
+            question.quesID = id;
+
+            db.questions.Add(question);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (questionExists(question.quesID))
+                {
+                    return Conflict();
+
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = question.quesID }, question);
+        }
+
+        // DELETE: api/questions/5
+        [ResponseType(typeof(question))]
+        public IHttpActionResult Deletequestion(int id)
+        {
+            question question = db.questions.Find(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            db.questions.Remove(question);
+            db.SaveChanges();
+
+            return Ok(question);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool questionExists(int id)
+        {
+            return db.questions.Count(e => e.quesID == id) > 0;
+        }
+    }
+}
---
+++ WebAPI/Scripts/app.js
@@ -0,0 +1,75 @@
+﻿var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function($scope, $http){
+    
+    $scope.selected = {};
+    $scope.message = "";
+    $scope.questions = "";
+
+    $http.get("/api/questions").then(function (result) {
+        $scope.questions = result.data;
+    });
+        $scope.getQuestions = function () {
+            $http.get("/api/questions").then(function (result) {
+                $scope.questions = result.data;
+            });
+        };
+
+        $scope.delQues = function (id) {
+            $http.delete("/api/questions/" + id).then(function (result) {
+                $scope.message = "Question Deleted";
+                $scope.getQuestions();
+            });
+        };
+
+        $scope.editQues = function (question) {
+            $scope.selected = angular.copy(question);
+        };
+
+        $scope.getTemplate = function (question) {
+            if (question.quesID === $scope.selected.quesID)
+                return 'Edit';
+            else
+                return 'Display';
+        };
+
+        $scope.reset = function () {
+            $scope.selected = {};
+        };
+
+        $scope.updateQues = function () {
+            var question = {
+                quesID: $scope.selected.quesID,
+                question1: $scope.selected.question1,
+                postedOn: $scope.selected.postedOn
+            };
+
+            var request = $http({
+                method: "put",
+                url: "/api/questions/" + $scope.selected.quesID,
+                data: question
+            });
+
+            request.then(function (data) {
+                $scope.message = "Question Updated";
+                $scope.reset();
+                $scope.getQuestions();
+            })
+        };
+
+        $scope.addQues = function () {
+            var q = {
+                question1: $scope.ques
+            };
+            var request = $http({
+                method: "post",
+                url: "/api/questions",
+                data: q
+            });
+            request.then(function (result) {
+                $scope.message = "Question Added";
+                $scope.getQuestions();
+            });
+        };
+
+});
